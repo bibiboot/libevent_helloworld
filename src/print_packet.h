6f6745ed26a4a1d819aa93292dc71581a38d6d9b
dan <rehman.d.fazlur@gmail.com>
2015-01-24 21:22:16 -0800

    Removing

--- src/print_packet.h
+++
@@ -1,7 +0,0 @@
-#include <linux/if_ether.h>
-#include "globals.h"
-
-char* print_human_format_number(uint16_t num, char *type);
-void print_routed_packet(struct sockaddr_in dest, char *result_if_name, char *src_mac, char *dest_mac);
-
-void print_data_detail (unsigned char* data , int size);
--- src/util.h
+++
@@ -1,8 +0,0 @@
-#include "globals.h"
-#include <linux/if_ether.h>
-
-uint64_t get_pattern(unsigned char *packet);
-
-void set_pattern(unsigned char *packet, uint64_t pttn_num);
-
-void create_log_file();
