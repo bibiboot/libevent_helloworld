b5854136dce27c15bf01e859724130d97b4eef9e
dan <rehman.d.fazlur@gmail.com>
2015-01-24 21:24:57 -0800

    Removing

--- src/filter.h
+++
@@ -1,9 +0,0 @@
-#include<netinet/ip.h>    //declarations for ip header
-#include <linux/if_ether.h>
-#include<netinet/ip_icmp.h>   //declarations for icmp header
-
-#include "globals.h"
-#include "print_packet.h"
-#include "util.h"
-
-bool is_allowed(unsigned char *packet);
--- src/globals.c
+++
@@ -1,7 +0,0 @@
-#include "globals.h"
-
-// Initializing the global structure with default values
-struct globals globals = {
-    .config = CONFIG_INIT,
-};
-
